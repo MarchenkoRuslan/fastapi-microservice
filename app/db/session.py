73330f72b92f3c8d89cf7c9bf4b42fe0ffe40f00
MarchenkoRuslan <n3mo07@bk.ru>
2025-01-06 10:52:08 +0200

    Refactor configuration to use environment variables for sensitive data
    - Updated the Settings class in config.py to utilize environment variables for PostgreSQL, Binance, and KYC service credentials, enhancing security and flexibility.
    - Simplified the database engine creation in session.py by defining DATABASE_URL using the updated settings.
    
    These changes improve the application's configuration management and promote best practices for handling sensitive information.

--- app/db/session.py
+++ app/db/session.py
@@ -2,12 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-# Создаем движок базы данных
-engine = create_engine(
-    f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}/{settings.POSTGRES_DB}"
-)
+DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}/{settings.POSTGRES_DB}"
 
-# Создаем фабрику сессий
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
